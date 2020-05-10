1243f03ac6bb585da286666b6536634224c08236
virginashmi15 <65062793+virginashmi15@users.noreply.github.com>
2020-05-10 01:16:26 -0700

    Add files via upload

---
+++ src/calculator.js
@@ -0,0 +1,32 @@
+function add(n1,n2){
+    let result = n1 + n2;
+    return result;
+}
+
+function sub(n1,n2){
+    let result = n1 - n2;
+    return result;
+}
+
+function mul(n1,n2){
+    let result = n1 * n2;
+    return result;
+}
+
+function div(n1,n2){
+let result = n1 / n2;
+    return result;
+}
+
+let addition_result = add(17,5)
+let subtraction_result = sub(10,5)
+let multiplication_result = mul(4,9)
+let division_result = div(7,5)
+
+console.log(`addition result: ${addition_result}`)
+console.log(`subtraction result: ${subtraction_result}`)
+console.log(` multiplication_result: ${multiplication_result}`)
+console.log(`division_result: ${division_result}`)
+
+
+
